3f42dce2fc46edf8b60b64033507a9c5e8d78979
Colin Marshall <colin.marshall94@gmail.com>
2023-04-09 10:53:25 -0700

    update: mobile styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Data, StyledData } from "./helpers/types";
 export default function App() {
   const dataImport: Data[] = data;
 
-  const styledDataArray: StyledData[] = dataImport.map((obj, i): StyledData => {
+  const styledDataArray: StyledData[] = dataImport.map((obj, i) => {
     const keyValue = i.toString();
     const key = { key: keyValue };
     const summaryContentContainer = {
@@ -14,13 +14,7 @@ export default function App() {
     const summaryContentName = {
       summaryContentName: "summaryContentName" + keyValue,
     };
-    const styledData = Object.assign(
-      obj,
-      key,
-      summaryContentContainer,
-      summaryContentName
-    );
-    return styledData;
+    return Object.assign(obj, key, summaryContentContainer, summaryContentName);
   });
 
   console.log(styledDataArray);
@@ -29,35 +23,49 @@ export default function App() {
     <>
       <main className="container">
         <section className="score">
-          <h1>Your Result</h1>
-          <p>76 of 100</p>
-          <h2>Great</h2>
-          <p>
-            You scored higher than 65% of the people who have taken these tests.
-          </p>
+          <h1 className="scoreHeader">Your Result</h1>
+          <div className="scoreCircle">
+            <h1 className="scoreCircleHeader">76</h1>
+            <p className="scoreCircleText">of 100</p>
+          </div>
+          <div className="scoreGrade">
+            <h2 className="scoreGradeHeader">Great</h2>
+            <p className="scoreBreakdown">
+              You scored higher than 65% of the people who have taken these
+              tests.
+            </p>
+          </div>
         </section>
         <section className="summary">
           <h1 className="summaryHeader">Summary</h1>
           {styledDataArray.map((content) => {
             return (
-              <div key={content.key} className={content.summaryContentContainer}>
-                <p className={content.summaryContentName}>{content.category}</p>
-                <p className="summaryScore">{content.score} / 100</p>
+              <div
+                key={content.key}
+                className={content.summaryContentContainer}
+              >
+                <div className={content.summaryContentName}>
+                  <img src={content.icon} alt={content.category} />
+                  <p>
+                    {content.category}
+                  </p>
+                </div>
+                <p >
+                  {content.score} <span className="summaryScore">/ 100</span>
+                </p>
               </div>
             );
           })}
-          <button type="submit" className="submit">
+          <button type="submit" className="continueBtn">
             Continue
           </button>
         </section>
       </main>
       <footer className="attribution">
         Challenge by{" "}
-        <span>
-          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-            Frontend Mentor
-          </a>
-        </span>
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
         . Coded by <a href="https://github.com/CascadeColin/">CascadeColin</a>.
       </footer>
     </>
